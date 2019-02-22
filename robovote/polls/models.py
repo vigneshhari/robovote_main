8702915d31611727db0108e8052aa4bb4c0d5923
Vignesh Hari <vichuhari100@gmail.com>
2019-02-22 20:43:14 +0530

    Ellam Theern

--- robovote/polls/models.py
+++ robovote/polls/models.py
@@ -6,7 +6,7 @@ class Team(models.Model):
     Bot = models.CharField(max_length = 10000)
     Members = models.CharField(max_length = 1000)
     Desc = models.TextField()
-    Backers = models.ManyToManyField('Backer' , null=True, blank=True)
+    Backers = models.ManyToManyField('Backer', blank=True)
  
     def __str__(self):
         return self.Name
