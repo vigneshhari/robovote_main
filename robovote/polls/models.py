6636702879315673fbaa114a53342d2d4dec2cf9
Vignesh Hari <vichuhari100@gmail.com>
2019-02-22 20:35:15 +0530

    Done

--- robovote/polls/models.py
+++ robovote/polls/models.py
@@ -6,7 +6,7 @@ class Team(models.Model):
     Bot = models.CharField(max_length = 10000)
     Members = models.CharField(max_length = 1000)
     Desc = models.TextField()
-    Backers = models.ManyToManyField('Backer')
+    Backers = models.ManyToManyField('Backer' , null=True, blank=True)
  
     def __str__(self):
         return self.Name
